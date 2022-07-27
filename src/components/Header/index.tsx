e2b04d11865cd773a5d149542a045f56f311d38a
darinadari13 <darina460@gmail.com>
2022-07-27 17:01:45 +0300

    innited project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.scss';
+export default function Header() {
+  return (
+    <div>
+      <h1 className='brand-name'>Easy Calorie Counter</h1>
+    </div>
+  ) 
+}
